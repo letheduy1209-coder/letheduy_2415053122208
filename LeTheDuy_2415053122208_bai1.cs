4c9f00ee26fe6ce83b3acc85fb72747f845c88f4
letheduy1209-coder <letheduy1209@gmail.com>
2026-03-09 13:31:32 +0700

    Create LeTheDuy_2415053122208_bai1.cs

---
+++ LeTheDuy_2415053122208_bai1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloCSharp.CSharp
+{
+    class VariableExample
+    {
+        static void Main(string[] args)
+        {
+            // Khai báo một hằng số có kiểu int.
+            // Bạn không thể gán giá trị mới cho hằng số.
+            const int MAX_SCORE = 100;
+
+            // Khai báo một biến có kiểu int.
+            int score = 0;
+
+            // Gán giá trị mới cho biến score.
+            score = 98;
+
+            // Khai báo một chuỗi (string)
+            string studentName = "LE THE DUL";
+
+            // In giá trị của biến ra màn hình Console.
+            Console.WriteLine("Hi, {0}", studentName);
+            Console.WriteLine("Your score: {0}/{1}", score, MAX_SCORE);
+
+            // Chờ người dùng nhập vào gì đó và nhấn Enter trước khi kết thúc chương trình
+            Console.ReadLine();
+        }
+    }
+}
